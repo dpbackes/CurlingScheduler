137787ee612e62270e5e5099d08a2c8b18f41747
Dan Backes <dbackes@articulate.com>
2023-01-04 16:17:57 -0600

    ability to shuffle weeks achieved

--- index.js
+++ index.js
@@ -84,5 +84,14 @@ const fitness = (schedule) => {
     return weeklyFitness
 }
 
+function shuffleArray(array) {
+    const shuffled = [].concat(array)
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+
+    return shuffled
+}
 console.dir(fitness(rrSchedule), {depth: null})
-console.dir(fitness(rrSchedule.map(week => week.sort(() => Math.random() - .5))), {depth: null})
+console.dir(fitness(rrSchedule.map(week => shuffleArray(week))), {depth: null})
